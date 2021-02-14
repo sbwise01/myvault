f0ba1c05287988a4ed80e52e355ea399104a51aa
Brad Wise <>
2021-02-14 16:13:13 -0600

    Update for tags standards

--- vault.tf
+++ vault.tf
@@ -18,11 +18,14 @@ module "vault" {
   lb_security_groups       = [module.aws_vpc.default_security_group.id]
   lb_subnet_ids            = [for subnet in module.aws_vpc.subnets["public"] : subnet.id]
   lb_acm_arn               = aws_acm_certificate.cert.arn
-  #depends_on               = [module.consul.asg_arn]
-  tag_name                = var.tag_name
-  tag_costcenter          = var.tag_costcenter
-  tag_environment         = var.tag_environment
-  tag_application         = var.tag_name
+
+  tags = {
+    CostCenter  = var.tag_costcenter
+    Name        = var.tag_name
+    Environment = var.tag_environment
+    Application = var.tag_name
+  }
+
   ca_certificate          = module.vault_certificate.ca_public_key
   certificate             = module.vault_certificate.ssc_public_key
   certificate_private_key = module.vault_certificate.ssc_private_key
